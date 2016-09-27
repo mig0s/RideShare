99ce54d5326dde72596579ee17d819e57501b97e
mig0s <mail@mig0s.tk>
2016-09-28 07:11:28 +0800

    RoutePlanner module added;

---
+++ modules/routeplanner/src/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php /**
+ * @file
+ * Contains \Drupal\route_planner\Controller\DefaultController.
+ */
+
+namespace Drupal\route_planner\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Default controller for the route_planner module.
+ */
+class DefaultController extends ControllerBase {
+
+}
